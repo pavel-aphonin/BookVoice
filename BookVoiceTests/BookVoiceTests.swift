376054e2c3da90ec2c99218486d5e1fdd20e3519
Pavel Aphonin <pavel.aphonin@gmail.com>
2026-02-22 21:35:58 +0300

    Initial Commit

---
+++ BookVoiceTests/BookVoiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookVoiceTests.swift
+//  BookVoiceTests
+//
+//  Created by Павел Афонин on 22.02.2026.
+//
+
+import Testing
+@testable import BookVoice
+
+struct BookVoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
